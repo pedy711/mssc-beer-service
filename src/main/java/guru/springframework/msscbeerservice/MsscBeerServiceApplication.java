b7fa9645658045b5ea1136c47dff85d5136abf9e
middoors <pedram.khoshdani@middoors.com>
2021-09-10 02:12:10 +0200

    adding bootstrap, closes #7

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,9 +2,8 @@ package guru.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication (exclude = { DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
