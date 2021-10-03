30100f32415daac1df5cde4b2c05aa055fde4185
middoors <pedram.khoshdani@middoors.com>
2021-10-03 22:09:56 +0200

    adding methods for controller - closes #15

---
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.services;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto getById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+}
