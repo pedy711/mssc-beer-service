e7b11163de07de351cfe069630c362532bc35aca
middoors <pedram.khoshdani@middoors.com>
2021-10-03 21:52:53 +0200

    Adding jsoncreator example - closes #14

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbeerservice.web.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -11,16 +12,16 @@ import java.util.List;
 public class BeerPagedList extends PageImpl<BeerDto> {
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-    public BeerPagedList(@JsonCreator("content") List<BeerDto> content,
-                         @JsonCreator("number") int number,
-                         @JsonCreator("size") int size,
-                         @JsonCreator("totalElements") Long totalElements,
-                         @JsonCreator("pageable") JsonNode pageable,
-                         @JsonCreator("last") boolean last,
-                         @JsonCreator("totalPages") int totalPages,
-                         @JsonCreator("sort") JsonNode sort,
-                         @JsonCreator("first") boolean first,
-                         @JsonCreator("numberOfElements") int numberOfElements) {
+    public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalElements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
         super(content, PageRequest.of(number, size), totalElements);
     }
 
